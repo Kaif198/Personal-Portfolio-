28cbf1153a390bcb87338e30d7411a427f32bf27
Kaif198 <kaifahmed6864@gmail.com>
2026-03-16 08:32:09 +0530

    Remove all except index and photo

--- tests/setup.js
+++
@@ -1,9 +0,0 @@
-import { readFileSync } from 'fs';
-import { resolve, dirname } from 'path';
-import { fileURLToPath } from 'url';
-import * as cheerio from 'cheerio';
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
-export const html = readFileSync(resolve(__dirname, '../index.html'), 'utf-8');
-export const $ = cheerio.load(html);
